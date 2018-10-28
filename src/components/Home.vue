214422319e708f03e6b94799bd9612c8831f57d0
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-10-28 03:39:08 +0100

    bonds audit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,18 +14,43 @@
             <div v-show="error">
               {{ error }}
             </div>
-            <div v-show="loading">
-              <h1 style="color:white" class="loancardwrapper">Loans</h1>
+            <div v-show="!loading">
+              <h1 style="color:white" class="loancardwrapper">Unsecured Corporate Bonds</h1>
               <div class=" mxauto loancardwrapper" v-for="loan in loans" :key="loan.contract">
                 
-                <b-card v-bind:border-variant="loan.color" >
-                     {{ loan.borrower }} {{loan.principal}}   <b-badge pill variant="primary">Primary</b-badge>
+                <b-card  footer-tag="footer">
+                  <b-row>
+                    <b-col cols=4>
+                      <b-card bg-variant="light"
+                            header="Audit"
+                            class="text-center">
+                        <p class="card-text">
+                          <b-progress :value="loan.rating" :max="20" variant="secondary"></b-progress>
+                          <br>
+                            <button type="button" class="btn btn-secondary btn-sm">{{ loan.auditor }}  </button>
+                        </p>
+                      </b-card>
 
-                     <br>
-                     {{ loan.rating }} - {{ loan.auditor }}
-                  <br>
-                 
-                   <b-button href="#" variant="primary">Information</b-button>
+                    </b-col>
+                    <b-col cols="8">
+                     <h3>Bond  <b-badge variant="info">{{loan.principal}}  ♦</b-badge></h3> 
+                     <h5>Interests  <b-badge>{{loan.interestRate/100}}  %</b-badge>  </h5>
+                      <br><br>
+                      <div slot="" class="text-right" align-v="end"><i>Days left to bit {{Math.floor(loan.timeLeft/8640)}} </i></div>
+                      <div slot="" class="text-right" align-v="end">                  
+                          <b-button href="#" variant="info" size="sm" >Information</b-button>
+                          <b-button href="#" variant="danger" size="sm" :disabled="!loan.timeLeft">Bid</b-button>
+                          <b-button href="#" variant="warning" size="sm" disabled>Transfert</b-button>
+                      </div>
+                    </b-col>
+                    <!-- <b-col>
+
+                    </b-col> -->
+
+                  </b-row>
+
+
+
                 </b-card>
               </div>
             </div>
@@ -41,197 +66,199 @@
 </template>
 
 <script>
-  import LoanRegistry from '../helper/loanRegistryHelper'
-  import bond from '../helper/bondHelper'
+import LoanRegistry from "../helper/loanRegistryHelper";
+import bond from "../helper/bondHelper";
+import grades from "../helper/grades";
 // import func from './vue-temp/vue-editor-bridge';
-  
-  export default {
-    name: 'home',
-    methods: {     
-    },
-    beforeCreate: async function () {
-        this.error = this.loansAddresses = null
-        // await LoanRegistry.init()
 
-        // this.loansLength = await LoanRegistry.getLoansLength()
-        // this.loansAddresses = await LoanRegistry.getLoans(0)
-        // console.log(this.loansAddresses)        
-    },
-    beforeMount: function () {
-        this.loans = []; // weirdly useful
-        this.loans[0] = {
-          contract: "0x59d4d9c24cd9517724bce24666bde8aeb27b7d54",
-          lender : null,
-          borrower : 7890,
-          borrowerAddress: "0xc66bD3780C297Baa1d910923c8b47Dce4b284076",
-          principal: 80808288,// in cents
-          bidTimeFrame: 88855888,
-          color: "",
-          amountRepaid: 0,
-          complete: false,
-          defaulted : false,
-          creationDate: 1540685256529,
-          duration : 2,
-          interestRate: 434,
-          granted: false,
-          rating: 14,
-          auditor: "S&P",
-        }
-        this.loans[1] = {
-          contract: "0x475e843c9771e287b65b7189b95d7261edcee1c8",
-          lender : null,
-          borrower : 38523,
-          borrowerAddress: "0x8D55ac7cFD01d50D48F0C3B257584181e144B51F",
-          principal: 3432,
-          bidTimeFrame: 55534332,
-          amountRepaid: 0,
-          color:"",
-          complete: false,
-          defaulted : false,
-          creationDate: 1540685256509,
-          duration : 4,
-          interestRate: 234,
-          granted: false,
-          rating: 20,
-          auditor: "His Grandma",
-        }
+export default {
+  name: "home",
+  methods: {},
+  beforeCreate: async function() {
+    this.error = this.loansAddresses = null;
+    // await LoanRegistry.init()
 
-        this.loans[2] = {
-          contract: "0x89b95d7261edcee1c8475e843c9771e287b65b71",
-          lender : 898983773,
-          borrower : 38523,
-          borrowerAddress: "0xD48F0C3B257584181e144B51F8D55ac7cFD01d50",
-          principal: 735433,
-          bidTimeFrame: 34234,
-          amountRepaid: 0,
-          color:"secondary",
-          complete: false,
-          defaulted : false,
-          creationDate: 1540685255509,
-          duration : 4,
-          interestRate: 234,
-          granted: false,
-          rating: 12,
-          auditor: "Registered Auditor Associates",
-        }
+    // this.loansLength = await LoanRegistry.getLoansLength()
+    // this.loansAddresses = await LoanRegistry.getLoans(0)
+    // console.log(this.loansAddresses)
+  },
+  beforeMount: function() {
+    this.loans = []; // weirdly useful
+    this.loans[0] = {
+      contract: "0x59d4d9c24cd9517724bce24666bde8aeb27b7d54",
+      lender: null,
+      borrower: 7890,
+      borrowerAddress: "0xc66bD3780C297Baa1d910923c8b47Dce4b284076",
+      principal: 80808288, // in cents
+      bidTimeFrame: 88855888,
+      timeLeft: 200300,
+      color: "",
+      amountRepaid: 0,
+      complete: false,
+      defaulted: false,
+      creationDate: 1540685256529,
+      duration: 2,
+      interestRate: 434,
+      granted: false,
+      rating: 14,
+      auditor: "S&P"
+    };
+    this.loans[1] = {
+      contract: "0x475e843c9771e287b65b7189b95d7261edcee1c8",
+      lender: null,
+      borrower: 38523,
+      borrowerAddress: "0x8D55ac7cFD01d50D48F0C3B257584181e144B51F",
+      principal: 3432,
+      bidTimeFrame: 55534332,
+      amountRepaid: 0,
+      timeLeft: 20300,
+      color: "",
+      complete: false,
+      defaulted: false,
+      creationDate: 1540685256509,
+      duration: 4,
+      interestRate: 234,
+      granted: false,
+      rating: 20,
+      auditor: "His Grandma"
+    };
 
-        this.loans[3] = {
-          contract: '0xb65b7189b95d7261edcee1c8475e843c9771e287',
-          lender: null,
-          borrower: 38383883,
-          borrowerAddress: '0xD48F0C3B257584181e144B51F8D55ac7cFD01d50',
-          principal: 3432,
-          bidTimeFrame: 2,
-          amountRepaid: 0,
-          color: 'danger',
-          complete: false,
-          defaulted: false,
-          creationDate: 1540685236509,
-          duration: 4,
-          interestRate: 234,
-          granted: false,
-          rating: 5,
-          auditor: 'Some Auditor'
-        }
+    this.loans[2] = {
+      contract: "0x89b95d7261edcee1c8475e843c9771e287b65b71",
+      lender: 898983773,
+      borrower: 38523,
+      borrowerAddress: "0xD48F0C3B257584181e144B51F8D55ac7cFD01d50",
+      principal: 735433,
+      bidTimeFrame: 34234,
+      amountRepaid: 0,
+      timeLeft: 0,
 
-      let self = this
-      setTimeout(function () {
-        self.loading = false
-        console.log('left loading', self.loading)
-      }, 3000)
+      color: "secondary",
+      complete: false,
+      defaulted: false,
+      creationDate: 1540685255509,
+      duration: 4,
+      interestRate: 234,
+      granted: false,
+      rating: 12,
+      auditor: "Registered Auditors"
+    };
 
-      setTimeout(function () {
-        console.log('Loading is now ', self.loading)
-      }, 4000)
-        // for (let i = 0; i < this.loansAddresses.length; i++) {
-        //   this.loans[i] = await bond.getthis.loansAddresses[i];
-        // }
-    },
-    mounted: function () {
+    this.loans[3] = {
+      contract: "0xb65b7189b95d7261edcee1c8475e843c9771e287",
+      lender: null,
+      borrower: 38383883,
+      borrowerAddress: "0xD48F0C3B257584181e144B51F8D55ac7cFD01d50",
+      principal: 3432,
+      bidTimeFrame: 2,
+      amountRepaid: 0,
+      timeLeft: 0,
 
-    },
-    components: {
-    },
-    computed: {
-        // colorLoan: function () {
-        // console.log(this)
-        // const now = + new Date()
-        // console.log(this.creationDate + this.bidTimeFrame - now)
-        // console.log(this)
-        // if(this.creationDate + this.bidTimeFrame - now>0){
-        //   return "";
-        // } else 
-        // if (this.lender) {
-        //   return "primary"
-        // } else {
-        //   return "danger"
-        // }
-      // }
-    },
-    data () {
-      return {
-        currentPage: 1,
-        loading: true,
-        error: null,
-        loansAddresses: null,
-        // loansLength:null,
-        loans:[],
-        registry: null
-      }
-    }
+      color: "danger",
+      complete: false,
+      defaulted: false,
+      creationDate: 1540685236509,
+      duration: 4,
+      interestRate: 234,
+      granted: false,
+      rating: 5,
+      auditor: "Some Auditor"
+    };
+
+    let self = this;
+    setTimeout(function() {
+      self.loading = false;
+      console.log("left loading", self.loading);
+    }, 3000);
+
+    setTimeout(function() {
+      console.log("Loading is now ", self.loading);
+    }, 4000);
+    // for (let i = 0; i < this.loansAddresses.length; i++) {
+    //   this.loans[i] = await bond.getthis.loansAddresses[i];
+    // }
+  },
+  mounted: function() {},
+  components: {},
+  computed: {
+    // colorLoan: function () {
+    // console.log(this)
+    // const now = + new Date()
+    // console.log(this.creationDate + this.bidTimeFrame - now)
+    // console.log(this)
+    // if(this.creationDate + this.bidTimeFrame - now>0){
+    //   return "";
+    // } else
+    // if (this.lender) {
+    //   return "primary"
+    // } else {
+    //   return "danger"
+    // }
+    // }
+  },
+  data() {
+    return {
+      currentPage: 1,
+      loading: true,
+      error: null,
+      loansAddresses: null,
+      // loansLength:null,
+      loans: [],
+      registry: null
+    };
   }
+};
 </script>
 
 <style scoped>
-  #home {
-    width: 100%;
-    /* height: 100vh; */
-  }
-  #listBody {
-    background-color: rgba(0,0,0, 0.2); 
-    height: 100%;
-    min-height: 100vh;
-    /* radial-gradient(#17a2b8, #FFF, #FFF); */
-    
-  }
-  .loancardwrapper {
-    width:100%;
-    margin: 1rem;
-  }
+#home {
+  width: 100%;
+  /* height: 100vh; */
+}
+#listBody {
+  background-color: rgba(0, 0, 0, 0.2);
+  height: 100%;
+  min-height: 100vh;
+  /* radial-gradient(#17a2b8, #FFF, #FFF); */
+}
+.loancardwrapper {
+  width: 100%;
+  margin: 1rem;
+}
 
-  /* ## Spinner ## */
-  .loading-ripple {
+/* ## Spinner ## */
+.loading-ripple {
   display: inline-block;
   position: relative;
   width: 64px;
   height: 64px;
-  }
-  .loading-ripple div {
-    position: absolute;
-    border: 4px solid #fff;
+}
+.loading-ripple div {
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: loading-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.loading-ripple div:nth-child(2) {
+  animation-delay: -0.9s;
+}
+@keyframes loading-ripple {
+  0% {
+    top: 28px;
+    left: 28px;
+    width: 0;
+    height: 0;
     opacity: 1;
-    border-radius: 50%;
-    animation: loading-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
   }
-  .loading-ripple div:nth-child(2) {
-    animation-delay: -0.9s;
+  100% {
+    top: -1px;
+    left: -1px;
+    width: 58px;
+    height: 58px;
+    opacity: 0;
   }
-  @keyframes loading-ripple {
-    0% {
-      top: 28px;
-      left: 28px;
-      width: 0;
-      height: 0;
-      opacity: 1;
-    }
-    100% {
-      top: -1px;
-      left: -1px;
-      width: 58px;
-      height: 58px;
-      opacity: 0;
-    }
-  }
+}
 
 .cross-background {
   position: fixed;
@@ -239,9 +266,12 @@
   top: 0;
   left: 0;
   right: 0;
-  background: 
- -webkit-linear-gradient(-45deg, rgba(250, 250, 250, 0.9) 45%, rgba(55, 230, 235, 1) 0%);
+  background: -webkit-linear-gradient(
+    -45deg,
+    rgba(250, 250, 250, 0.9) 45%,
+    rgba(55, 230, 235, 1) 0%
+  );
 
-z-index:-2;
+  z-index: -2;
 }
 </style>
