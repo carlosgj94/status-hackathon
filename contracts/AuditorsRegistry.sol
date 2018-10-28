e0a63949ff5649549a9b17e30e9201e690733fa3
Xavier Lavayssière <xavier.lavayssiere@gmail.com>
2018-10-28 07:29:23 +0100

    added registry of auditors

---
+++ contracts/AuditorsRegistry.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.24;
+
+/**
+ * Author: xaxa 
+ */
+
+/** @title Auditors Registry. */
+/** @dev Allows to self register as an auditor, and validators to consolidate your position */
+
+contract AuditorsRegistry {
+    mapping(address => string) public auditors;
+    mapping(address => address[]) public validations;
+
+    function selfRegistration(string _name) public {
+        auditors[msg.sender]= _name;
+    }
+
+    function getAuditor(address _addr) public view returns(string) {
+        return auditors[_addr];
+    }
+
+    function validateAuditor(address _addr) public {
+      validations[_addr].push(msg.sender);
+    }
+
+    function getValidator(address _addr, uint _index) public view returns(address) {
+        return validations[_addr][_index];
+    }
+
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var LoanRegistry = artifacts.require('./LoanRegistry.sol')
+var AuditorsRegistry = artifacts.require('./AuditorsRegistry.sol')
 
 module.exports = function (deployer) {
   deployer.deploy(LoanRegistry)
+  deployer.deploy(AuditorsRegistry)
 }
